8602a6fcbf456c7d43b1887f73acc5bb9e815d46
claramaravel <masterprodutos2022@gmail.com>
2025-03-01 14:50:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-// Função para abrir o modal e exibir a imagem clicada
-function openModal(src) {
-    var modal = document.getElementById("myModal");
-    var modalImg = document.getElementById("img01");
-    modal.style.display = "block";
-    modalImg.src = src;
-}
-
-// Função para fechar o modal
-function closeModal() {
-    var modal = document.getElementById("myModal");
-    modal.style.display = "none";
-}
-
-// Fechar o modal se o usuário clicar fora da imagem
-window.onclick = function(event) {
-    var modal = document.getElementById("myModal");
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
+// Função para abrir o link de pagamento
+function openPayment() {
+    // Substitua o link abaixo pelo seu link real de pagamento
+    window.location.href = "https://go.tribopay.com.br/9bphewugrc";
+  }
+  
